3ada3e84d5108105e5731570119378f0374a2c34
Steven De Avila <stevejd88@gmail.com>
2019-09-04 10:55:51 -0500

    created createModals() function

--- js/app.js
+++ js/app.js
@@ -22,30 +22,28 @@ Promise.all([
   employees = data[0];
   generateHTML(employees);
 })
-// .then( data =>  {
-//     return info = data;
-// });
+
 
 // ---------------------------------------------------------------------------
 // FUNCTION CREATING HTML FROM DATA IN API REQUEST
 // ---------------------------------------------------------------------------
 function generateHTML(data) {
-   info = data.results;
-   data.results.forEach(person => {
+  info = data.results;
+
+  for (let i = 0; i < info.length; i++) {
      const section = document.createElement('section');
      employeeList.appendChild(section);
-     const html= `<div class="card">
-     <img src="${person.picture.large}" alt="${person.name.first} ${person.name.last}">
+     const html = `<div class="card" id="user-${[i]} ">
+     <img src="${info[i].picture.large}" alt="${info[i].name.first} ${info[i].name.last}">
      <div class="info">
-       <h2>${person.name.first} ${person.name.last}</h2>
-       <span>${person.email}</span>
-       <span>${person.location.city}</span>
+       <h2>${info[i].name.first} ${info[i].name.last}</h2>
+       <span>${info[i].email}</span>
+       <span>${info[i].location.city}</span>
      </div>
      </div>`;
      // console.log(html);
      section.innerHTML = html;
-   });
-
+   };
 }
 
 
@@ -56,29 +54,35 @@ function generateHTML(data) {
 const modal = document.getElementById("myModal");
 // Get the card that opens the modal
 const card = document.querySelector('.card');
-// Get the <span> element that closes the modal
 const modalContent = document.querySelector('.modal-content');
 
 // const birthday = function() {
 //   const day = info[0].dob.date;
 //   console.log(day);
 // }
+function createModals() {
+  for (let i = 0; i < info.length; i++) {
+    const section = document.createElement('section');
+    modalContent.appendChild(section);
+    const html =
+      `<div class="modalUser">
+          <img src="${info[i].picture.large}" alt="${info[i].name.first} ${info[i].name.last}">
+          <p>${info[i].name.first} ${info[i].name.last}<p>
+          <p>${info[i].email}</p>
+          <p>${info[i].location.city}</p>
+          <hr>
+          <p>${info[i].phone}</p>
+          <p>${info[i].location.street} , ${info[i].location.state} ${info[i].location.postcode}</p>
+      </div>`;
+      section.innerHTML = html;
+  }
+};
 
-const displayModal = () => {
-    modal.style.display = "block";
-    for (let i = 0; i < info.length; i++) {
-        modalContent.innerHTML =
-          `<div class="modalUser">
-              <img src="${info[i].picture.large}" alt="${info[i].name.first} ${info[i].name.last}">
-              <p>${info[i].name.first} ${info[i].name.last}<p>
-              <p>${info[i].email}</p>
-              <p>${info[i].location.city}</p>
-              <hr>
-              <p>${info[i].phone}</p>
-              <p>${info[i].location.street} , ${info[i].location.state} ${info[i].location.postcode}</p>
-          </div>`;
-    }
+function displayModal() {
+  modal.style.display = "block";
+  createModals();
 }
+
 
 // When the user clicks on the card, open the modal
 document.addEventListener('click', (e) => {
@@ -100,7 +104,8 @@ document.addEventListener('click', (e) => {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target === modal) {
+  if (event.target.className === 'modal' || event.target.className === 'modal-content') {
     modal.style.display = "none";
+    modalContent.innerHTML = '';
   }
 }
